f87e8d51c5a5a95e72ff0610592dffb8ceabab29
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-03-16 19:25:13 +0530

    Authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import userModel from "../models/user.model.js";
 import crypto from "crypto";
 import jwt from "jsonwebtoken";
 import config from "../config/config.js";
-import { log } from "console";
+
 
 
 export async function register(req, res) {
