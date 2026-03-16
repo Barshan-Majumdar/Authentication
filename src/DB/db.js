c877c8e4d0175fdd276a86941a0cc696d0b073be
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-03-16 12:56:51 +0530

    Authentication

---
+++ src/DB/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import config from "../config/config.js";
+async function connectDB() {
+    await mongoose.connect(config.MONGO_URI)
+    console.log("MongoDB Connected");
+    
+}
+
+export default connectDB
